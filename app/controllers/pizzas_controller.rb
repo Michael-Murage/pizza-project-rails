a6703923162e1a921a0fbcee16812590217378b8
Michael <mickeymurage@gmail.com>
2022-10-01 07:22:15 +0300

    added index and show methods to pizza and restaurant controllers

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,6 +1,19 @@
 class PizzasController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 	def index
 		render json: Pizza.all, status: :ok
+	end
+
+	def show
+		piz = Pizza.find(params[:id])
+		render json: piz, status: :ok
+	end
+
+	
+
+	private
+
+	def render_not_found
+		render json: {error: "We could not find what you were looking for"}, status: :not_found
 	end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,17 @@
 class RestaurantsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 	def index
 		render json: Restaurant.all, status: :ok
+	end
+
+	def show
+		rest = Restaurant.find(params[:id])
+		render json: rest, serializer: RestaurantShowSerializer, status: :ok
+	end
+
+	private
+
+	def render_not_found
+		render json: {error: "Restaurant not found"}
 	end
 end

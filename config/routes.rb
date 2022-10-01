1c31f4adabd978a6d6b50da5e6856e9881d3f3e3
Michael <mickeymurage@gmail.com>
2022-10-01 07:26:23 +0300

    added routes to accomodate the controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [:index]
-  resources :pizzas, only: [:index]
+  resources :restaurants, only: [:index, :show]
+  resources :pizzas, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

8875704c548fac46fd237ea618bb08ddec9e9665
Michael <mickeymurage@gmail.com>
2022-10-04 01:22:04 +0300

    attempting to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 	get '/pizza_only', to: 'pizza_only#index'
 	get '/rest_only', to: 'rest_only#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
